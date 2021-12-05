9f57f5aeb0e25a192d7874382c94c8537f8454f4
Shellb1 <56370289+Shellb1@users.noreply.github.com>
2021-12-05 15:32:43 -0600

    Test Cases for Account

---
+++ Alexandria/src/test/java/accountTests/CreateAccountTest.java
@@ -0,0 +1,82 @@
+package accountTests;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class CreateAccountTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "lib/mac/chromedriver");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testCreateAccount() throws Exception {
+    driver.get("http://ec2-18-217-199-195.us-east-2.compute.amazonaws.com:8080/Alexandria/Account.jsp");
+    driver.findElement(By.id("inputEmail")).click();
+    driver.findElement(By.id("inputEmail")).clear();
+    driver.findElement(By.id("inputEmail")).sendKeys("createAccount@unomaha.edu");
+    driver.findElement(By.id("inputUsernameId")).clear();
+    driver.findElement(By.id("inputUsernameId")).sendKeys("createAccountUser");
+    driver.findElement(By.id("inputPasswordId")).clear();
+    driver.findElement(By.id("inputPasswordId")).sendKeys("createAccountPassword");
+    driver.findElement(By.id("createAccountButton")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
